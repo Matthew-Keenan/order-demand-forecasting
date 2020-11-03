f03ad18ef8065ff641318c51ead60d3acf686cc4
Matt Keenan <matthewk1313@gmail.com>
2020-11-02 21:22:18 -0500

    Added scatterplot to display some of the data

--- OrderDemandForecasting.py
+++ OrderDemandForecasting.py
@@ -3,15 +3,16 @@ import pandas as pd
 import sklearn
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plot 
 
 # Reads csv file into pandas dataframe
-postData = pd.read_csv("Daily_Demand_Forecasting_Orders.csv", sep=";")
+orderData = pd.read_csv("Daily_Demand_Forecasting_Orders.csv", sep=";")
 
 # Sets variable for the data column being predicted
 value_to_predict = "Total orders"
 
 # Creates numpy array with all columns except the one being predicted
-x = np.array(postData.drop([value_to_predict], 1))
+x = np.array(orderData.drop([value_to_predict], 1))
 
 # x array values were set in scientific notation, set here to float values
 np.set_printoptions(suppress=True)
@@ -20,7 +21,7 @@ np.set_printoptions(suppress=True)
 x = x.astype(int)
 
 # Creates numpy arrray with just the column containing post likes
-y = np.array(postData[value_to_predict])
+y = np.array(orderData[value_to_predict])
 
 # Splits x and y datasets: x array training values, x array testing values, y array training values, and y array testing values
 # Separates 10% of each dataset to be used for testing
@@ -43,3 +44,13 @@ print(f"Mean Squared Error (MSE): {round(MSE, 6)}")
 print()
 for i in range(len(y_predict) - 1):
     print(f"Predicted Orders: {round(y_predict[i], 2)} \t Actual Given Orders: {round(y_test[i], 2)}" )
+
+# Set what data from dataset will be displayed on x and y axes
+y_axis = orderData[value_to_predict]
+x_axis = orderData["Order type B"]
+
+# Plots data from given columns, labels the axes, and displays the scatterplot
+plot.scatter(x_axis, y_axis)
+plot.xlabel("Order Type B")
+plot.ylabel("Total Orders")
+plot.show()
